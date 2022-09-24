511c86d947826fc84041fa6c53097e2a6a3383ce
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-09-24 15:35:52 +0500

    RightInfoBar added

--- src/components/RightInfoBar.jsx
+++ src/components/RightInfoBar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function RightInfoBar() {
   return (
-    <div className="RightInfoBar">
-      {/* <p  className='text-light'>Right info bar</p> */}
+    <div className='RightInfoBar d-flex flex-column'>
+      <a href = "mailto: ahmadjajja86@gmail.com" className='verticalMail text-decoration-none text-white-50 hover me-2'>ahmadjajja86@gmail.com</a>
+      <div className="vl border-end border-2 border-secondary"></div>
     </div>
   )
 }
