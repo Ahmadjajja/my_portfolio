47f1dfa76acc24c5fa0848adaf6298f08ba0866f
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-09-24 14:45:27 +0500

    LeftInfoBar completed!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Navbar from "./components/navbar/Navbar"
 import "./App.scss"
 import "bootstrap/dist/js/bootstrap.bundle";
+import LeftInfoBar from "./components/LeftInfoBar";
+import RightInfoBar from "./components/RightInfoBar";
 function App() {
   return (
     <div className="App" style={{backgroundColor: "#0A192F"}}>
           <Navbar/>
+          <LeftInfoBar />
+          <RightInfoBar />
     </div>
   );
 }
---
+++ src/components/RightInfoBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RightInfoBar() {
+  return (
+    <div className="RightInfoBar">
+      {/* <p  className='text-light'>Right info bar</p> */}
+    </div>
+  )
+}
+
+export default RightInfoBar
