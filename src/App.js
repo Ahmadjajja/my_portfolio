ef4b0719648b65070414e934eef7f7af0e32c664
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-09-24 11:57:51 +0500

    Navbar 80% completedx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./components/navbar/Navbar"
 import "./App.scss"
+import "bootstrap/dist/js/bootstrap.bundle";
 function App() {
   return (
     <div className="App" style={{backgroundColor: "#0A192F"}}>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,36 +3,40 @@ import React from 'react'
 function Navbar() {
   return (
     <div class="Navbar">
-      <nav class="navbar navbar-expand-lg navbar-dark " >
-        <div class="container-fluid d-flex space-between py-2">
-          <div>
-            <a class="navbar-brand" href="#">Navbar</a>
-          </div>
-          <div>
+      <nav className="navbar navbar-expand-lg navbar-dark " >
+        <div className="container-fluid d-flex space-between pb-2 pt-3 px-5">
+          {/* <div className="text-center"> */}
+            <a className="navbar-brand border border-success border-2 py-2 rounded-circle btn fw-light text-success" href="#"><span>AJ</span></a>
+          {/* </div> */}
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse  navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav  mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link" href="#"><span class="mainColor" style={{color: "#87F2A5"}}>01. </span>About</a>
+          {/* <div> */}
+            {/* <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation"> */}
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+              <ul className="navbar-nav  mb-2 mb-lg-0">
+                <li className="nav-item me-2">
+                  <a className="nav-link fw-light text-light hover" href="#"><span className="text-success" >01. </span>About</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#"><span class="mainColor" style={{color: "#87F2A5"}}>02. </span> Experience</a>
+                <li className="nav-item me-2">
+                  <a className="nav-link fw-light text-light hover" href="#"><span className="text-success" >02. </span> Experience</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#"> <span class="mainColor" style={{color: "#87F2A5"}}>03. </span> Work</a>
+                <li className="nav-item me-2">
+                  <a className="nav-link fw-light text-light hover" href="#"> <span className="text-success" >03. </span> Work</a>
                 </li>
-                <li class="nav-item">
-                  
-                  <a class="nav-link" href="#"><span class="mainColor" style={{color: "#87F2A5"}}>04. </span> Contact</a>
+                <li className="nav-item me-2">
+
+                  <a className="nav-link fw-light text-light hover" href="#"><span className="text-success" >04. </span> Contact</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#"><span class="mainColor" style={{color: "#87F2A5"}}>05. </span> Blog</a>
+                <li className="nav-item me-2">
+                  <a className="nav-link fw-light text-light hover" href="#"><span className="text-success" >05. </span> Blog</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link btn  text-success border border-success rounded-pill" href="#"><span className="px-2">Resume</span></a>
                 </li>
               </ul>
             </div>
-          </div>
+          {/* </div> */}
         </div>
       </nav>
     </div>
