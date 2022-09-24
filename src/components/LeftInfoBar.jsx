c6793dcbe1d7b68b5610f81c6eb7be3b9fcd5aa8
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-09-24 15:12:31 +0500

    LeftInfoBar 100% completed

--- src/components/LeftInfoBar.jsx
+++ src/components/LeftInfoBar.jsx
@@ -3,12 +3,12 @@ import { FiGithub , FiTwitter, FiLinkedin, FiFacebook} from 'react-icons/fi'
 import { FaFacebookF} from 'react-icons/fa'
 function LeftInfoBar() {
   return (
-    <div className='LeftInfoBar text-light d-flex flex-column'>
-      <a href="https://github.com/Ahmadjajja?tab=overview&from=2022-06-01&to=2022-06-30" className='text-light fs-5 '><FiGithub/></a>
-      <a href="https://twitter.com/AhmadJajja786" className='text-light fs-5 mt-3'><FiTwitter/></a>
-      <a href="https://www.linkedin.com/in/ahmad-jajja-ba8b34210/" className='text-light fs-5 mt-3'><FiLinkedin/></a>
-      <a href="https://www.facebook.com/ahmad.jajja.9081/" className='text-light fs-5 mt-3'><FaFacebookF/></a>
-      <div className="vl border-start border-1 border-light"></div>
+    <div className='LeftInfoBar d-flex flex-column'>
+      <a href="https://github.com/Ahmadjajja?tab=overview&from=2022-06-01&to=2022-06-30" className='text-white-50 iconHover fw-light fs-5 '><FiGithub/></a>
+      <a href="https://twitter.com/AhmadJajja786" className='text-white-50 iconHover fs-5 mt-3'><FiTwitter/></a>
+      <a href="https://www.linkedin.com/in/ahmad-jajja-ba8b34210/" className='text-white-50 iconHover fs-5 mt-3'><FiLinkedin/></a>
+      <a href="https://www.facebook.com/ahmad.jajja.9081/" className='text-white-50 iconHover fs-5 mt-3'><FaFacebookF/></a>
+      <div className="vl border-start border-2 border-secondary"></div>
     </div>
   )
 }
